a2d602c325a726c656fcf1b77ebe6fd0aa1ae11f
doruk <dorukbulutinfo@gmail.com>
2023-03-07 01:00:57 +0300

    Create User signup logic

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,40 @@
 import express from "express";
+import "express-async-errors";
+import { currentUserRouter } from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import mongoose from "mongoose";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there !");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
 });
 
-app.listen(3000, () => {
-  console.log("Auth Service Listening on PORT: 3000");
-});
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to mongodb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Auth Service Listening on PORT: 3000");
+  });
+};
+
+start();
